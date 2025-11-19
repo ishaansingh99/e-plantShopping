38a6ac7667dbe249ecb39d3488559e2d0d3d4056
Ishaan Singh <ishaans1999@gmail.com>
2025-11-19 13:57:31 -0500

    finished project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
